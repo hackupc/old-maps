e5cd816638b3963d24726a89d4e367ad2a41c208
Gerard del Castillo <gerarddelcasti@hotmail.com>
2017-10-08 04:11:33 +0200

    add imaginbank room

--- js/app.js
+++ js/app.js
@@ -184,6 +184,10 @@ document.addEventListener("DOMContentLoaded", function(){
         renderer = new THREE.WebGLRenderer({antialias: true});
         renderer.setSize( window.innerWidth, window.innerHeight );
         renderer.setClearColor(0xffffff);
+        //rgb values in blender are not gamma corrected
+        renderer.gammaOutput = true;
+        renderer.gammaInput = true;
+
         controls = new THREE.OrbitControls(camera, renderer.domElement)
 
         map = loadFromStorage("map");
@@ -596,16 +600,23 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     function initLights(){
-        var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.8 );
+        hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
         hemiLight.color.setHSL( 0.6, 1, 0.6 );
         hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-        hemiLight.position.set( 0, 500, 0 );
+        hemiLight.position.set( 0, 50, 0 );
+        scene.add( hemiLight );
 
-        var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+        //
+
+        dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
         dirLight.color.setHSL( 0.1, 1, 0.95 );
         dirLight.position.set( -1, 1.75, 1 );
-        dirLight.position.multiplyScalar( 50 );
+        dirLight.position.multiplyScalar( 30 );
+        scene.add( dirLight );
+
+/*
         dirLight.castShadow = true;
+
         dirLight.shadow.mapSize.width = 2048;
         dirLight.shadow.mapSize.height = 2048;
         var d = 50;
@@ -613,11 +624,10 @@ document.addEventListener("DOMContentLoaded", function(){
         dirLight.shadow.camera.right = d;
         dirLight.shadow.camera.top = d;
         dirLight.shadow.camera.bottom = -d;
+
         dirLight.shadow.camera.far = 3500;
         dirLight.shadow.bias = -0.0001;
-
-        scene.add( hemiLight );
-        scene.add( dirLight );
+*/
     }
 
     function initMain(){
